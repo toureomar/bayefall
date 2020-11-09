a5a2fa1e5a95f86e465dff51dac499af4354fc17
touréomar <ngalzo38@gmail.com>
2020-11-09 15:57:07 +0000

    tester mes exos

--- exo1.c
+++ exo1.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    int i;
-    
+    int n,i;
     
     printf("saisir un nombre \n");
-    scanf("%d",&i);
-    do
+    scanf("%d",&n);
+    for (i=n; i>0; i--)
     {
         printf("afficher les autres nombres %d\n",i);
-    } while (i<10,i--);
-
+    }
+    
     return 0;
 }
